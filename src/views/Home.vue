28ec8005e728d37bd50b7b82854effdf823aefa6
Piccio <lorenzo.hl.picchi@gmail.com>
2025-03-03 12:01:43 +0100

    example img set

---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>Benvenuto nel nostro sito vetrina</h1>
+        <p>Scopri i nostri servizi e prodotti</p>
+        <img src="@/assets/clinical-psychology-doctor-with-patient-on-couch-3847440743.jpg" alt="psychologist img" />
+    </div>
+</template>
