9a834d9b6d8e6c6e5c6b30f110f9dad08016725f
Piccio <lorenzo.hl.picchi@gmail.com>
2025-03-03 12:13:12 +0100

    Contacts page basic layout set

---
+++ src/views/Contact.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>
+            Contact us!
+        </h1>
+        <form @submit.prevent="sendMessage">
+            <input type="text" v-model="name" placeholder="Your name" />
+            <textarea v-model="message" placeholder="Your message" required></textarea>
+            <button type="submit">Send</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return {
+                name: '',
+                message: ''
+            };
+        },
+        methods: {
+            sendMessage(){
+                alert(`Hello ${this.name}, your message is: ${this.message}`);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
