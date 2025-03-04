d343dbad97af9323a221d55e16e8a3d0d6dc7af5
Piccio <lorenzo.hl.picchi@gmail.com>
2025-03-04 23:10:42 +0100

    import Bootstrap CSS and JS in main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import { createApp } from 'vue'
-import './styles/style.css'
 import App from './App.vue'
 import router from './router'
 import './styles/main.scss'
 
+// Importing Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+
+// Importing Axios
+// import axios from 'axios';
+// import VueAxios from 'vue-axios';
+
+
+
 
 
 createApp(App).use(router).mount('#app');
