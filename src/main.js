2aebfcd1923001e4d2b956534cfd50790efa5cff
Piccio <lorenzo.hl.picchi@gmail.com>
2025-03-04 22:53:14 +0100

    add Bootstrap CSS and JS along with Bootstrap Icons to main.js

--- src/main.js
+++ src/main.js
@@ -4,4 +4,9 @@ import App from './App.vue'
 import router from './router'
 import './styles/main.scss'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+
 createApp(App).use(router).mount('#app');
