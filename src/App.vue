fecce5babe0ee1d8444613aaa3e04f7f895dfdb7
Piccio <lorenzo.hl.picchi@gmail.com>
2025-03-04 22:53:30 +0100

    implement responsive navigation bar with Bootstrap in AppNavBar component

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,14 @@
 </script>
 
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/contact">Contact</router-link> |
-  </nav>
+
   <router-view />
 </template>
 
 <style>
-  nav {
+  .navbar {
+    position: fixed;
+    top: 0;
     display: flex;
     gap: 10px;
     background-color: #f1f1f1;
---
+++ src/components/AppNavBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container">
+                <router-link to="/" class="navbar-brand">Dr. Mario Rossi</router-link>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <router-link to="/" class="nav-link">Home</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link to="/services" class="nav-link">Servizi</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link to="/contact" class="nav-link">Contatti</router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+header {
+    margin-bottom: 20px;
+}
+</style>
